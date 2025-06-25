b197bfb031bca34504f4237323360f81d504c6d9
Hyunwoo Park <hwpark0826@naver.com>
2025-06-25 15:22:52 +0900

    start_date를 한국시간으로 변경

--- utils/date_filter.py
+++ utils/date_filter.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta, timezone
 import pandas as pd
-from db.mongo_handler import load_mongo_data
 
 def filter_date(df):
     # 날짜 컬럼을 datetime으로 변환
@@ -12,9 +11,9 @@ def filter_date(df):
     # 오늘 자정과 7일 전 자정 계산
     today = datetime.now(KST).date()
     start_date = today - timedelta(days=6)  # 오늘 포함 7일
-    start_datetime = datetime.combine(start_date, datetime.min.time())
+    start_datetime = datetime.combine(start_date, datetime.min.time(), tzinfo=KST)
 
-     # 필터링: start_date ~ 오늘 자정까지 (시간 포함)
-    df = df[(df["actionTime"] >= start_datetime)]
+    # 필터링: start_date ~ 오늘 자정까지 (시간 포함)
+    df = df[df["actionTime"] >= start_datetime]
 
     return df
